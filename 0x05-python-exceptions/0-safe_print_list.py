7cfe3e0eb585dd40dd404ae9f1f6589c897a0fb5
willykar <wilsonnjenga799@gmail.com>
2023-12-18 08:03:48 -0800

    New files

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    printed = 0
+    for a in range(0, x):
+        try:
+            print("{}".format(my_list[a]), end="")
+            printed += 1
+        except:
+            continue
+    print()
+    return printed

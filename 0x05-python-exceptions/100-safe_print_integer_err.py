7e8885497a1355d2f6b00ab6eb8e998807a86fe6
willykar <wilsonnjenga799@gmail.com>
2023-12-18 09:10:43 -0800

    New file

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    """
+    A function that prints an integer
+    The integer should be printed followed by a new line
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)

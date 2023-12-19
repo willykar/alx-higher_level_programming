b1c05961c3c2a6411def89d813546691c4763625
willykar <wilsonnjenga799@gmail.com>
2023-12-19 09:38:12 -0800

    New file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module for class square"""
+
+
+class Square:
+    """Represent a class called square"""
+
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

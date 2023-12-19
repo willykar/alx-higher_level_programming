0edee66d4abefadfd47e34390570bfa1954c3f54
willykar <wilsonnjenga799@gmail.com>
2023-12-19 10:12:48 -0800

    New file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A module for class square"""
+
+
+class Square:
+    """Represent a class called square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''method that returns area'''
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the # characters"""
+        for a in range(0, self.__size):
+            [print("#", end="") for b in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")

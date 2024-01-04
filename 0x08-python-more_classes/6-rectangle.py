8cb82349fdc45c744365aeac55001d72f0a595dd
willykar <wilsonnjenga799@gmail.com>
2024-01-04 10:17:29 -0800

    update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """ Class Rectangle with private width and height"""
 
-     num_of_instances = 0
+    num_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """

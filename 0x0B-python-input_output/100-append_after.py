0c4bce54805e449a160cfc05fee55a26fb8183fc
willykar <wilsonnjenga799@gmail.com>
2024-01-09 08:45:17 -0800

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """append_after module"""
 
+
 def append_after(file_name="", p_search_tring="", p_new_string=""):
     """A function that inserts a line of text to a file"""
     with open(file_name, "r") as f:

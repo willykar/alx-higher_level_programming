5307e6aec58d434d9758fb257ac71486f78a1e97
willykar <wilsonnjenga799@gmail.com>
2024-01-10 10:28:29 -0800

    update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,6 +8,12 @@ def matrix_mul(m_a, m_b):
     Arguments:
         m_a (list of lists of ints/floats): The first matrix
         m_b (list of lists of ints/floats): The second matrix
+
+    Raises:
+        TypeError: If either m_a or m_b is not a list of lists of ints or floats
+        TypeError: If either m_a or m_b is empty
+        TypeError: If either m_a or m_b has different-sized rows.
+        ValueError: If m_a and m_b cannot be multiplied
     Return:
         A matrix representing the multiplication of m_a by m_b
     """

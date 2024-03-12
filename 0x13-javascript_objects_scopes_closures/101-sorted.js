dd8ba382541e8aee3eaae7468d0dc40c6338c0e2
willykar <wilsonnjenga799@gmail.com>
2024-03-12 13:16:28 -0700

    New file

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const uniqVal = [...new Set(vals)];
+const newDict = {};
+for (const i in uniqVal) {
+  const list = [];
+  for (const j in totalist) {
+    if (totalist[j][i] === uniqVal[j]) {
+      list.unshift(totalist[j][0]);
+    }
+  }
+  newDict[valsUniq[i]] = list;
+}
+console.log(newDict);

345dffa7861ad8e311241251f971b903788140c3
willykar <wilsonnjenga799@gmail.com>
2024-03-15 04:32:38 -0700

    update

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a script that takes in an argument and displays all values in the states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    usr_input = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (usr_input, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()

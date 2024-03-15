288434dd905a0199af0057157bd417f5a0487490
willykar <wilsonnjenga799@gmail.com>
2024-03-15 11:23:46 -0700

    update

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" a script that prints the first State object from the database hbtn_0e_6_usa """
+""" a script that prints the first State object from the
+database hbtn_0e_6_usa """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker

6940fe0f7d7b796b99ec0418f92845927f53490d
willykar <wilsonnjenga799@gmail.com>
2024-03-15 14:25:49 -0700

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class city(Base):
     """inherits from Base
     Attributes:
@@ -14,7 +15,6 @@ class city(Base):
     name (string)
     state_id (string)
     """
-
     __tablename__ = cities
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)

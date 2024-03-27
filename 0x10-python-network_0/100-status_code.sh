c53bd315bd5d9f8643356c6eeca0ccb2e28b01a2
willykar <103494595+willykar@users.noreply.github.com>
2024-03-28 01:51:10 +0300

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | grep -i "^HTTP" | awk '{print $2}'
+curl -sI "$1" -w '%{response_code}' -o /dev/null

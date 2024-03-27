41b759f210c0b97f5c760c88546bfa81668982f2
willykar <103494595+willykar@users.noreply.github.com>
2024-03-28 02:56:00 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,18 +12,12 @@ def find_peak(list_of_integers):
     Return:
         int(the peak integer(s))
     """
-    if len(list_of_integers) == 0:
-        return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        return max(list_of_integers)
-
-    mid = int(len(list_of_integers) / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+  start = 0
+  end = len(list_of_integers) - 1
+  while start < end:
+      mid = (start + end) // 2
+      if list_of_integers[mid] < list_of_integers[mid + 1]:
+          start = mid + 1
+      else:
+          end = mid
+  return start

4b21f185dda95ea9e2f81a87c7114a9dac403e48
willykar <wilsonnjenga799@gmail.com>
2024-03-27 17:01:37 -0700

    update_peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,12 +12,12 @@ def find_peak(list_of_integers):
     Return:
         int(the peak integer(s))
     """
-  start = 0
-  end = len(list_of_integers) - 1
-  while start < end:
-      mid = (start + end) // 2
-      if list_of_integers[mid] < list_of_integers[mid + 1]:
-          start = mid + 1
-      else:
-          end = mid
-  return start
+    start = 0
+    end = len(list_of_integers) - 1
+    while start < end:
+        mid = (start + end) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            start = mid + 1
+        else:
+            end = mid
+    return start

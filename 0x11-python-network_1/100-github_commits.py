aff0f0a39ed39945a6f3b7e85586798c2ce1b9b3
willykar <wilsonnjenga799@gmail.com>
2024-03-30 13:53:04 -0700

    github_commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a Python script that takes 2 arguments and lists
+10 commits (from the most recent to oldest) of a repository"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    req = requests.get(url)
+    commit = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass

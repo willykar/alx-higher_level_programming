2b6f4c5cb93c52d7ca2b21100630526e7335aa75
willykar <wilsonnjenga799@gmail.com>
2024-03-30 12:49:29 -0700

    status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a status module to fetch an url"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")

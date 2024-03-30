3a40389d9d047bc80abe2a7fad022d91c71e3a4d
willykar <103494595+willykar@users.noreply.github.com>
2024-03-30 23:57:48 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ a status module to fetch an url"""
-import urllib.request
+from import urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    req = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(req) as response:
-        data = response.read().decode('utf-8')
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = Request(url)
+    with urlopen(req) as response:
+        data = response.read()
 
     print("Body response:")
     print(f"\t- type: {type(data)}")
     print(f"\t- content: {data}")
-    print(f"\t- utf8 content: {data}")
+    print(f"\t- utf8 content: {data.decode("utf-8"}")

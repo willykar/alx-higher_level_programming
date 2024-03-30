032b951c1d3aa41d569b564259d760466630e506
willykar <wilsonnjenga799@gmail.com>
2024-03-30 13:11:04 -0700

    error_code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request
+to the URL and displays the body of the response"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print(f"Error code: {req.status_code}")
+    else:
+        print("req.text")

0dddbb7c2dbefdcf4f71531eb2b4424be0d90ea8
willykar <103494595+willykar@users.noreply.github.com>
2024-04-23 22:07:07 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,32 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-if (!url || !filePath) {
-  console.error('Usage: ./5-request_store.js <URL> <FILE_PATH>');
-  process.exit(1);
-}
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error: Unexpected status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing file:', err);
-      process.exit(1);
-    }
-    console.log(`Content successfully written to ${filePath}`);
-  });
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));

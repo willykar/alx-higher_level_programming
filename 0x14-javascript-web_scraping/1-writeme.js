b599c9306b5a7d58403ef481bcf439ca575a9481
willykar <103494595+willykar@users.noreply.github.com>
2024-04-23 22:55:04 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,11 +6,6 @@ const file = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Error: a filepath and content is needed');
-  process.exit(1);
-}
-
 file.writeFile(filePath, content, 'utf-8', (error) => {
   if (error) {
     console.error('Error writing file:', error);

97b0ea5e052f7ab724c57783fd84c6437cc9af4c
willykar <103494595+willykar@users.noreply.github.com>
2024-04-23 22:52:50 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
 // A sctipt that writes a string to a file
 
-const fs = require('fs');
+const file = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], function (err) {
-  if (err) {
-    return (console.log(err));
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+if (!filePath || !content) {
+  console.error('Error: a filepath and content is needed');
+  process.exit(1);
+}
+
+file.writeFile(filePath, content, 'utf-8', (error) => {
+  if (error) {
+    console.error('Error writing file:', error);
+  } else {
+    console.log('Successfully wrote content to file!');
   }
 });
